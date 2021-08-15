4d44d171e19eae105f2f70d8590c0c67e3d8ff16
JosephZhou19 <josephzhou1234@gmail.com>
2021-08-15 10:07:59 -0400

    Add System and Weekly
    Redid Add system with modal and started working on a weekly schedule

--- calendar.js
+++ calendar.js
@@ -1,4 +1,5 @@
 const date = new Date();
+let wDate = new Date();
 const renderCalendar = () =>{
 date.setDate(1);
 const monthDays = document.querySelector(".days");
@@ -42,12 +43,69 @@ for(let j = 1; j <= nextDays; j++){
 	monthDays.innerHTML = days;
 }
 };
-document.querySelector(".prev").addEventListener("click", ()=>{
+const renderWeek = () =>{
+    const weekDays = document.querySelector(".weekDate");
+const lastDay = new Date(wDate.getFullYear(), wDate.getMonth() + 1, 0).getDate();
+let firstDayIndex = wDate.getDay();
+let lastDayIndex = new Date(wDate.getFullYear(), wDate.getMonth() + 1, 0).getDay();
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  document.querySelector(".today h1").innerHTML = months[wDate.getMonth()];
+ document.querySelector(".today p").innerHTML = new Date().toDateString();
+
+ let days = "<th></th>";
+ for (let i = 1; i <= lastDay; i++) {
+    if(i === new Date().getDate()){
+        firstDayIndex = new Date(wDate.getFullYear(), wDate.getMonth(), wDate.getDate() - wDate.getDay());
+        lastDayIndex = new Date(wDate.getFullYear(), wDate.getMonth(), wDate.getDate()- wDate.getDay() + 7);
+        break;
+    }
+ }
+  document.querySelector(".weekLength").innerHTML = firstDayIndex.toDateString() + " - " + lastDayIndex.toDateString();
+ wDate = firstDayIndex;
+ while(wDate.getDate() != lastDayIndex.getDate()){
+    days += `<th>${wDate.getDate()}</th>`
+    wDate = new Date(wDate.getFullYear(), wDate.getMonth(), wDate.getDate() + 1);
+ }
+weekDays.innerHTML = days;
+wDate = firstDayIndex;
+};
+document.querySelector(".week .prev").addEventListener("click", ()=>{
+    wDate.setDate(wDate.getDate() - 1)    
+    renderWeek();
+});
+document.querySelector(".week .next").addEventListener("click", ()=>{
+    wDate.setDate(wDate.getDate() + 7)    
+    renderWeek();
+});
+document.querySelector(".month .prev").addEventListener("click", ()=>{
 	date.setMonth(date.getMonth() - 1);
 	renderCalendar();
 });
-document.querySelector(".next").addEventListener("click", ()=>{
+document.querySelector(".month .next").addEventListener("click", ()=>{
 	date.setMonth(date.getMonth() + 1);
 	renderCalendar();
 });
+
 renderCalendar();
+renderWeek();
+document.querySelector(".swap button").addEventListener("click", ()=>{
+    document.querySelector(".calendar").style.display = "none";
+    document.querySelector(".weekCalendar").style.display = "block";
+});
+document.querySelector(".swapBack button").addEventListener("click", ()=>{
+    document.querySelector(".weekCalendar").style.display = "none";
+    document.querySelector(".calendar").style.display = "block";
+});
--- tasks.js
+++ tasks.js
@@ -1,6 +1,6 @@
-const input = document.querySelector('input');
-const btn = document.querySelector(".addTask > button")
-
+//const input = document.querySelector('input');
+const btn = document.getElementById("addBtn")
+const input = document.querySelector(".taskName > input")
 btn.addEventListener('click', addList);
 input.addEventListener('keyup', (e)=>{
 	(e.keyCode == 13 ? addList(e) : null);
@@ -12,16 +12,38 @@ function addList(e){
 	const checkBtn = document.createElement("button");
 	const delBtn = document.createElement("button");
 	const undoBtn = document.createElement("button");
+	const time = document.createElement("text");
+	const description = document.createElement("text");
 	checkBtn.innerHTML = '<i class="fa fa-check"></i>'
 	delBtn.innerHTML = '<i class="fa fa-trash"></i>'
 	undoBtn.innerHTML = '<i class="fa fa-undo"></i>'
-	if(input.value !==''){
+	if(input.value !=='' ){
 		newLi.textContent = input.value;
 		input.value ='';
+		var radios = document.getElementsByName('color');
+
+		for (var i = 0, length = radios.length; i < length; i++) {
+  		if (radios[i].checked) {
+    	// do whatever you want with the checked radio
+    		newLi.style.background = radios[i].value;
+    	// only one radio can be logically checked, don't check the rest
+    	break;
+  		}
+		}
+		const dateTime = new Date(document.querySelector(".basics input[name='dateTime']").value);
+		time.innerHTML =dateTime.toLocaleTimeString();//.substring(dateTime.getTime().getIndex('t') + 1);
+		time.innerHTML += "-";
+		const duration =  document.querySelector(".basics input[name='length']").value;
+		var newDate = new Date(dateTime.getTime() + parseInt(duration)*3600000);
+		time.innerHTML += newDate.toLocaleTimeString()//.substring(newDate.getTime().getIndex('t') + 1);
+		console.log(time.innerHTML);
 		notCompleted.appendChild(newLi);
+		newLi.appendChild(time);
+		description.innerHTML = document.querySelector(".description > input").value;
+		newLi.appendChild(description);
 		newLi.appendChild(delBtn);
 		newLi.appendChild(checkBtn);
-
+		modal.style.display = "none";
 	}
 	checkBtn.addEventListener("click", function(){
 		const parent = this.parentNode;
@@ -42,3 +64,29 @@ function addList(e){
 		parent.appendChild(checkBtn);
 	})
 }
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var modalBtn = document.getElementById("modalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+modalBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
