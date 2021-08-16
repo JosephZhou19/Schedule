8d0cdf4b2db11988d152f4eeb6b6ee62dd282e71
JosephZhou19 <josephzhou1234@gmail.com>
2021-08-16 17:26:23 -0400

    Swap calendars
    Added swapping functionality between calendar types

--- calendar.js
+++ calendar.js
@@ -1,5 +1,6 @@
-const date = new Date();
+let date = new Date();
 let wDate = new Date();
+//Renders the calendar
 const renderCalendar = () =>{
 date.setDate(1);
 const monthDays = document.querySelector(".days");
@@ -23,7 +24,7 @@ const months = [
     "December",
   ];
  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-
+ document.querySelector(".date h1").innerHTML += " " + date.getFullYear();
  document.querySelector(".date p").innerHTML = new Date().toDateString();
  let days = "";
 for(let x = firstDayIndex; x > 0; x--){
@@ -34,7 +35,7 @@ for (let i = 1; i <= lastDay; i++) {
 	if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
 		days += `<div class="today">${i}</div>`;
 	} else{
-    	days += `<div>${i}</div>`;
+    	days += `<div class="day">${i}</div>`;
     	monthDays.innerHTML = days;
  	}
  }
@@ -42,7 +43,42 @@ for(let j = 1; j <= nextDays; j++){
 	days += `<div class = "next-date">${j}</div>`;
 	monthDays.innerHTML = days;
 }
+let x = document.getElementsByClassName("day");
+for(let i = 0; i < x.length; i++){
+    x[i].addEventListener("click", ()=>{
+        document.querySelector(".calendar").style.display = "none";
+        document.querySelector(".weekCalendar").style.display = "block";
+        wDate = new Date(date.getFullYear(), date.getMonth(), x[i].innerHTML);
+        renderWeek();
+    });
+}
+document.querySelector(".today").addEventListener("click", ()=>{
+        document.querySelector(".calendar").style.display = "none";
+        document.querySelector(".weekCalendar").style.display = "block";
+        wDate = new Date(date.getFullYear(), date.getMonth(), document.querySelector(".today").innerHTML);
+        renderWeek();
+    });
+let y = document.getElementsByClassName("prev-date");
+for(let i = 0; i < y.length; i++){
+    y[i].addEventListener("click", ()=>{
+        document.querySelector(".calendar").style.display = "none";
+        document.querySelector(".weekCalendar").style.display = "block";
+        wDate = new Date(date.getFullYear(), date.getMonth() - 1, y[i].innerHTML);
+        renderWeek();
+    });
+}
+let z = document.getElementsByClassName("next-date");
+for(let i = 0; i < z.length; i++){
+    z[i].addEventListener("click", ()=>{
+        document.querySelector(".calendar").style.display = "none";
+        document.querySelector(".weekCalendar").style.display = "block";
+        wDate = new Date(date.getFullYear(), date.getMonth() + 1, z[i].innerHTML);
+        renderWeek();
+    });
+}
 };
+
+//Renders the weekly calendar
 const renderWeek = () =>{
     const weekDays = document.querySelector(".weekDate");
 const lastDay = new Date(wDate.getFullYear(), wDate.getMonth() + 1, 0).getDate();
@@ -63,6 +99,7 @@ const months = [
     "December",
   ];
   document.querySelector(".today h1").innerHTML = months[wDate.getMonth()];
+  document.querySelector(".today h1").innerHTML += " " + date.getFullYear();
  document.querySelector(".today p").innerHTML = new Date().toDateString();
 
  let days = "<th></th>";
@@ -82,6 +119,7 @@ const months = [
 weekDays.innerHTML = days;
 wDate = firstDayIndex;
 };
+//sets up all the buttons between each week/month of the calendars
 document.querySelector(".week .prev").addEventListener("click", ()=>{
     wDate.setDate(wDate.getDate() - 1)    
     renderWeek();
@@ -98,14 +136,17 @@ document.querySelector(".month .next").addEventListener("click", ()=>{
 	date.setMonth(date.getMonth() + 1);
 	renderCalendar();
 });
-
 renderCalendar();
 renderWeek();
+//Allows for the swapping between calendar types
 document.querySelector(".swap button").addEventListener("click", ()=>{
     document.querySelector(".calendar").style.display = "none";
     document.querySelector(".weekCalendar").style.display = "block";
+    renderWeek();
 });
 document.querySelector(".swapBack button").addEventListener("click", ()=>{
     document.querySelector(".weekCalendar").style.display = "none";
     document.querySelector(".calendar").style.display = "block";
+    date = new Date(wDate.getFullYear(), wDate.getMonth(), 1);
+    renderCalendar();
 });
